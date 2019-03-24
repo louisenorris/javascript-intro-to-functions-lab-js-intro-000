6d52aec35d08f96c18f4c48d906ca7dfd7e634ac
Louise Norris <louisenorris73@yahoo.co.uk>
2019-03-24 09:56:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,4 +18,7 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase(string) === string) {
     return "I can't hear you!";
   }
+  else if (string.toUpperCase(string) === string) {
+    return "YES INDEED!";
+  }
 }
